1594a4b40c1696083bc268ad64f15e4670de88c9
Ryan Brott <rcbrott@gmail.com>
2018-07-09 09:01:57 -0700

    Add path panel

--- gui/src/main/kotlin/com/acmerobotics/splinelib/MainFrame.kt
+++ gui/src/main/kotlin/com/acmerobotics/splinelib/MainFrame.kt
@@ -1,14 +1,24 @@
 package com.acmerobotics.splinelib
 
 import java.awt.Dimension
+import javax.swing.BoxLayout
 import javax.swing.JFrame
+import javax.swing.JPanel
 
 class MainFrame : JFrame() {
     init {
         title = "Spline Designer"
-        size = Dimension(500, 500)
+        size = Dimension(1000, 600)
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
-        contentPane = PathPanel()
+        val panel = JPanel()
+        val panel2 = JPanel()
+        val layout = BoxLayout(panel, BoxLayout.LINE_AXIS)
+        panel.layout = layout
+        panel2.add(PathPanel())
+        panel.add(panel2)
+        panel.add(FieldPanel())
+        contentPane = JPanel()
+        contentPane.add(panel)
     }
 }
--- gui/src/main/kotlin/com/acmerobotics/splinelib/PathPanel.kt
+++ gui/src/main/kotlin/com/acmerobotics/splinelib/PathPanel.kt
@@ -1,55 +1,21 @@
 package com.acmerobotics.splinelib
 
-import com.acmerobotics.splinelib.path.QuinticSplineSegment
-import java.awt.*
-import java.awt.geom.AffineTransform
-import java.awt.geom.Point2D
-import java.io.File
-import javax.imageio.ImageIO
-import javax.swing.JPanel
-import kotlin.math.min
-import kotlin.math.roundToInt
+import java.awt.GridLayout
+import javax.swing.*
+
 
 class PathPanel : JPanel() {
-    private val spline = QuinticSplineSegment(
-            Waypoint(10.0, 20.0, 20.0, 10.0),
-            Waypoint(35.0, 50.0, -25.0, 12.0)
-    )
+    init {
+        layout = GridLayout(0, 4, 5, 0)
 
-    override fun paintComponent(g: Graphics?) {
-        super.paintComponent(g)
+        add(JLabel("X", SwingConstants.CENTER))
+        add(JLabel("Y", SwingConstants.CENTER))
+        add(JLabel("Heading", SwingConstants.CENTER))
+        add(JLabel("Reversed", SwingConstants.CENTER))
 
-        val g2d = g as Graphics2D
-
-        // antialiasing
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
-
-        // transform coordinate frame
-        val fieldSize = min(width, height)
-        val offsetX = (width - fieldSize) / 2.0
-        val offsetY = (height - fieldSize) / 2.0
-
-        val transform = AffineTransform()
-        transform.translate(width / 2.0, height / 2.0)
-        transform.scale(fieldSize / 144.0, -fieldSize / 144.0)
-        transform.rotate(Math.PI / 2)
-
-        // draw field
-        val fieldImage = ImageIO.read(File("gui/src/main/resources/transparent_field.png"))
-        g2d.drawImage(fieldImage, offsetX.roundToInt(), offsetY.roundToInt(), fieldSize, fieldSize, null)
-
-        g2d.stroke = BasicStroke(1.5F)
-
-        val displacements = (0..1000).map { it / 1000.0 * spline.length() }
-        for (i in 1..1000) {
-            val firstRawPoint = spline[displacements[i-1]]
-            val secondRawPoint = spline[displacements[i]]
-            val firstPoint = Point2D.Double()
-            val secondPoint = Point2D.Double()
-            transform.transform(Point2D.Double(firstRawPoint.x, firstRawPoint.y), firstPoint)
-            transform.transform(Point2D.Double(secondRawPoint.x, secondRawPoint.y), secondPoint)
-            g2d.drawLine(firstPoint.x.roundToInt(), firstPoint.y.roundToInt(), secondPoint.x.roundToInt(), secondPoint.y.roundToInt())
-        }
+        add(JTextField(7))
+        add(JTextField(7))
+        add(JLabel())
+        add(JCheckBox())
     }
 }
