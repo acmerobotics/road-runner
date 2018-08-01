dc106fd8650f9eccbe32ca2b3a3e4877b5d2026f
Ryan Brott <rcbrott@gmail.com>
2018-08-01 15:54:17 -0700

    Add PID documentation and dokka config

--- core/src/main/kotlin/com/acmerobotics/splinelib/control/PIDCoefficients.kt
+++ core/src/main/kotlin/com/acmerobotics/splinelib/control/PIDCoefficients.kt
@@ -1,3 +1,6 @@
 package com.acmerobotics.splinelib.control
 
+/**
+ * This class stores the proportional, integral, and derivative gains used by [PIDFController].
+ */
 class PIDCoefficients(val kP: Double = 0.0, val kI: Double = 0.0, val kD: Double = 0.0)
--- core/src/main/kotlin/com/acmerobotics/splinelib/control/PIDFController.kt
+++ core/src/main/kotlin/com/acmerobotics/splinelib/control/PIDFController.kt
@@ -3,13 +3,39 @@ package com.acmerobotics.splinelib.control
 import kotlin.math.sign
 
 /**
- * This class implements a PID controller with various feedforward components.
+ * PID controller with various feedforward components. [kV], [kA], and [kStatic] are designed for DC motor feedforward
+ * control (the most common kind of feedforward in FTC). [kF] provides a custom feedforward term for other plants.
+ *
+ * @param pid traditional PID coefficients
+ * @param kV feedforward velocity gain
+ * @param kA feedforward acceleration gain
+ * @param kStatic constant additive feedforward contribution
+ * @param kF custom, position-dependent feedforward (e.g., a cos term for arms)
  */
-class PIDFController(val pid: PIDCoefficients, val kV: Double = 0.0, val kA: Double = 0.0, val kStatic: Double = 0.0, val kF: (Double) -> Double = { 0.0 }) {
+class PIDFController @JvmOverloads constructor(
+        val pid: PIDCoefficients,
+        val kV: Double = 0.0,
+        val kA: Double = 0.0,
+        val kStatic: Double = 0.0,
+        val kF: (Double) -> Double = { 0.0 }
+) {
     private var errorSum: Double = 0.0
     private var lastUpdateTimestamp: Double = Double.NaN
+
+    /**
+     * Target position (i.e., the controller setpoint)
+     */
     var targetPosition: Double = 0.0
 
+    /**
+     * Run a single iteration of the controller.
+     *
+     * @param position current measured position (feedback)
+     * @param velocity feedforward velocity
+     * @param acceleration feedforward acceleration
+     * @param currentTimestamp timestamp for the above parameters (intendend for simulation)
+     */
+    @JvmOverloads
     fun update(position: Double, velocity: Double = 0.0, acceleration: Double = 0.0, currentTimestamp: Double = System.nanoTime() / 1e9): Double {
         return if (lastUpdateTimestamp.isNaN()) {
             lastUpdateTimestamp = currentTimestamp
@@ -25,6 +51,9 @@ class PIDFController(val pid: PIDCoefficients, val kV: Double = 0.0, val kA: Dou
         }
     }
 
+    /**
+     * Reset the controller's integral sum.
+     */
     fun reset() {
         errorSum = 0.0
         lastUpdateTimestamp = Double.NaN
