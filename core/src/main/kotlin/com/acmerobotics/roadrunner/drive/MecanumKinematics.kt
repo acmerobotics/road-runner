f1900244c10c7cad11085d886dafd5e581bc2bf9
Ryan Brott <rcbrott@gmail.com>
2018-08-23 18:07:43 -0700

    Unroll mecanum kinematics matrix operations

--- core/src/main/kotlin/com/acmerobotics/roadrunner/drive/MecanumKinematics.kt
+++ core/src/main/kotlin/com/acmerobotics/roadrunner/drive/MecanumKinematics.kt
@@ -1,15 +1,11 @@
 package com.acmerobotics.roadrunner.drive
 
 import com.acmerobotics.roadrunner.Pose2d
-import org.apache.commons.math3.linear.MatrixUtils
 
 /**
  * Mecanum drive kinematic equations. [Ether's paper](http://www.chiefdelphi.com/media/papers/download/2722) provides a
  * derivation.
  */
-// TODO: does unrolling mecanum kinematic matrix multiplies obscure intent?
-// the answer seems to be no for tank/differential
-// there is a non-negligible performance difference
 object MecanumKinematics {
 
     /**
@@ -20,24 +16,16 @@ object MecanumKinematics {
      * @param trackWidth lateral distance between pairs of wheels on different sides of the robot
      * @param wheelBase distance between pairs of wheels on the same side of the robot
      */
-    // TODO: it might be better to just pass K to better facilitate experimental results
-    // alternatively, they can just pass k as trackWidth
     @JvmStatic
     @JvmOverloads
     fun robotToWheelVelocities(robotPoseVelocity: Pose2d, trackWidth: Double, wheelBase: Double = trackWidth): List<Double> {
         val k = (trackWidth + wheelBase) / 2.0
-        val inverseKinematicsMatrix = MatrixUtils.createRealMatrix(
-                arrayOf(
-                        doubleArrayOf(1.0, -1.0, -k),
-                        doubleArrayOf(1.0, 1.0, -k),
-                        doubleArrayOf(1.0, -1.0, k),
-                        doubleArrayOf(1.0, 1.0, k)
-                )
+        return listOf(
+                robotPoseVelocity.x - robotPoseVelocity.y - k * robotPoseVelocity.heading,
+                robotPoseVelocity.x + robotPoseVelocity.y - k * robotPoseVelocity.heading,
+                robotPoseVelocity.x - robotPoseVelocity.y + k * robotPoseVelocity.heading,
+                robotPoseVelocity.x + robotPoseVelocity.y + k * robotPoseVelocity.heading
         )
-        val poseMatrix = MatrixUtils.createColumnRealMatrix(doubleArrayOf(
-                robotPoseVelocity.x, robotPoseVelocity.y, robotPoseVelocity.heading))
-        val wheelVelocities = inverseKinematicsMatrix.multiply(poseMatrix)
-        return wheelVelocities.transpose().data[0].toList()
     }
 
     /**
@@ -48,9 +36,9 @@ object MecanumKinematics {
      * @param trackWidth lateral distance between pairs of wheels on different sides of the robot
      * @param wheelBase distance between pairs of wheels on the same side of the robot
      */
-    // follows from linearity of the derivative
     @JvmStatic
     @JvmOverloads
+    // follows from linearity of the derivative
     fun robotToWheelAccelerations(robotPoseAcceleration: Pose2d, trackWidth: Double, wheelBase: Double = trackWidth) =
             robotToWheelVelocities(robotPoseAcceleration, trackWidth, wheelBase)
 
@@ -65,15 +53,10 @@ object MecanumKinematics {
     @JvmOverloads
     fun wheelToRobotVelocities(wheelVelocities: List<Double>, trackWidth: Double, wheelBase: Double = trackWidth): Pose2d {
         val k = (trackWidth + wheelBase) / 2.0
-        val forwardKinematicsMatrix = MatrixUtils.createRealMatrix(
-                arrayOf(
-                        doubleArrayOf(0.25, 0.25, 0.25, 0.25),
-                        doubleArrayOf(-0.25, 0.25, -0.25, 0.25),
-                        doubleArrayOf(-0.25 / k, -0.25 / k, 0.25 / k, 0.25 / k)
-                )
-        )
-        val wheelVelocityMatrix = MatrixUtils.createColumnRealMatrix(wheelVelocities.toDoubleArray())
-        val poseVelocities = forwardKinematicsMatrix.multiply(wheelVelocityMatrix).transpose().data[0]
-        return Pose2d(poseVelocities[0], poseVelocities[1], poseVelocities[2])
+        return Pose2d(
+                wheelVelocities.sum(),
+                wheelVelocities[1] + wheelVelocities[3] - wheelVelocities[0] - wheelVelocities[2],
+                (wheelVelocities[2] + wheelVelocities[3] - wheelVelocities[0] - wheelVelocities[1]) / k
+        ) * 0.25
     }
 }
