8783caaa54f522d03956681bbbc8671917e72fde
Ryan Brott <rcbrott@gmail.com>
2019-12-11 23:28:04 -0500

    Display the area swept by by the robot body

--- gui/src/main/kotlin/com/acmerobotics/roadrunner/gui/FieldPanel.kt
+++ gui/src/main/kotlin/com/acmerobotics/roadrunner/gui/FieldPanel.kt
@@ -3,14 +3,14 @@ package com.acmerobotics.roadrunner.gui
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import java.awt.*
-import java.awt.geom.AffineTransform
-import java.awt.geom.Point2D
+import java.awt.geom.*
 import javax.imageio.ImageIO
 import javax.swing.JPanel
 import kotlin.math.min
 import kotlin.math.roundToInt
 
-private const val RESOLUTION = 1000
+private const val RESOLUTION = 100
+private const val ROBOT_SIZE = 18.0
 
 /**
  * Panel displaying an image of the field with the trajectory/path superimposed.
@@ -46,11 +46,11 @@ class FieldPanel : JPanel() {
         val offsetX = (width - fieldSize) / 2.0
         val offsetY = (height - fieldSize) / 2.0
 
-        val transform = AffineTransform()
-        transform.translate(width / 2.0, height / 2.0)
-        transform.scale(fieldSize / 144.0, fieldSize / 144.0)
-        transform.rotate(Math.PI / 2)
-        transform.scale(-1.0, 1.0)
+        val fieldTrans = AffineTransform()
+        fieldTrans.translate(width / 2.0, height / 2.0)
+        fieldTrans.scale(fieldSize / 144.0, fieldSize / 144.0)
+        fieldTrans.rotate(Math.PI / 2)
+        fieldTrans.scale(-1.0, 1.0)
 
         // draw field
         val fieldImage = ImageIO.read(javaClass.getResource("/field.png"))
@@ -59,10 +59,11 @@ class FieldPanel : JPanel() {
         g2d.color = SERIES_COLORS[2]
 
         // draw poses
+        val circle = Ellipse2D.Double(0.0, 0.0, 3.0, 3.0)
         for (pose in poses) {
-            val point = Point2D.Double()
-            transform.transform(Point2D.Double(pose.x, pose.y), point)
-            g2d.fillArc(point.x.roundToInt() - 5, point.y.roundToInt() - 5, 10, 10, 0, 360)
+            circle.x = pose.x - circle.width / 2
+            circle.y = pose.y - circle.height / 2
+            g2d.fill(fieldTrans.createTransformedShape(circle))
         }
 
         if (trajectory == null) return
@@ -77,15 +78,36 @@ class FieldPanel : JPanel() {
         val displacements = (0..RESOLUTION).map {
             it / RESOLUTION.toDouble() * (trajectory?.duration() ?: 1.0)
         }
+
+        val robotTrans = AffineTransform()
+        val robotRect = Rectangle2D.Double(-ROBOT_SIZE / 2, -ROBOT_SIZE / 2, ROBOT_SIZE, ROBOT_SIZE)
+
+        val firstPoint = Point2D.Double()
+        val secondPoint = Point2D.Double()
+        val line = Line2D.Double()
         for (i in 1..RESOLUTION) {
             val firstPose = trajectory?.get(displacements[i - 1]) ?: Pose2d()
             val secondPose = trajectory?.get(displacements[i]) ?: Pose2d()
-            val firstPoint = Point2D.Double()
-            val secondPoint = Point2D.Double()
-            transform.transform(Point2D.Double(firstPose.x, firstPose.y), firstPoint)
-            transform.transform(Point2D.Double(secondPose.x, secondPose.y), secondPoint)
-            g2d.drawLine(firstPoint.x.roundToInt(), firstPoint.y.roundToInt(),
-                secondPoint.x.roundToInt(), secondPoint.y.roundToInt())
+
+            fieldTrans.transform(Point2D.Double(firstPose.x, firstPose.y), firstPoint)
+            fieldTrans.transform(Point2D.Double(secondPose.x, secondPose.y), secondPoint)
+            line.setLine(firstPoint, secondPoint)
+
+            // draw the path segment
+            g2d.draw(line)
         }
+
+        g2d.paint = Color(SERIES_COLORS[2].red, SERIES_COLORS[2].green, SERIES_COLORS[2].blue, 120)
+        val area = Area()
+        for (i in 0..RESOLUTION) {
+            val firstPose = trajectory?.get(displacements[i]) ?: Pose2d()
+
+            // draw the robot rect
+            robotTrans.setTransform(fieldTrans)
+            robotTrans.translate(firstPose.x, firstPose.y)
+            robotTrans.rotate(firstPose.heading)
+            area.add(Area(robotTrans.createTransformedShape(robotRect)))
+        }
+        g2d.fill(area)
     }
 }
