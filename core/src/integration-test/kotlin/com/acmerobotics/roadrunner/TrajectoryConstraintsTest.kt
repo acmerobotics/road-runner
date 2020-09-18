37107b467f9a5cd33f0311403d8bfeff83d8113d
Ryan Brott <rcbrott@gmail.com>
2020-09-18 16:00:05 -0700

    Bump Gradle, Kotlin, and Detekt

--- core/src/integration-test/kotlin/com/acmerobotics/roadrunner/TrajectoryConstraintsTest.kt
+++ core/src/integration-test/kotlin/com/acmerobotics/roadrunner/TrajectoryConstraintsTest.kt
@@ -2,18 +2,12 @@ package com.acmerobotics.roadrunner
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.acmerobotics.roadrunner.path.PathContinuityViolationException
-import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
-import com.acmerobotics.roadrunner.util.DoubleProgression
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.knowm.xchart.QuickChart
-import org.knowm.xchart.style.MatlabTheme
 import java.util.*
 import kotlin.math.PI
-import kotlin.math.max
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TrajectoryConstraintsTest {
