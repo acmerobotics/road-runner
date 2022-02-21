448a3adb2d2f821aed9e42998ef216ca4732f690
Ryan Brott <rcbrott@gmail.com>
2022-02-20 19:42:15 -0500

    Fix tank drive power track width

--- core/src/main/kotlin/com/acmerobotics/roadrunner/drive/TankDrive.kt
+++ core/src/main/kotlin/com/acmerobotics/roadrunner/drive/TankDrive.kt
@@ -88,7 +88,7 @@ abstract class TankDrive constructor(
     }
 
     override fun setDrivePower(drivePower: Pose2d) {
-        val powers = TankKinematics.robotToWheelVelocities(drivePower, 1.0)
+        val powers = TankKinematics.robotToWheelVelocities(drivePower, 2.0)
         setMotorPowers(powers[0], powers[1])
     }
 
