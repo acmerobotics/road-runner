b5108138c117365218e9c82376f3a54516b8e5f9
Smartyguy001 <109079347+Smartyguy001@users.noreply.github.com>
2023-09-10 13:14:29 +0300

    Added SwerveKinematics to Kinematics.kt
    Added SwerveKinematics to Kinematics.kt using the SwerveKinematics in Roadrunner 0.5.x as reference

--- core/src/main/kotlin/com/acmerobotics/roadrunner/Kinematics.kt
+++ core/src/main/kotlin/com/acmerobotics/roadrunner/Kinematics.kt
@@ -77,6 +77,75 @@ data class MecanumKinematics @JvmOverloads constructor(
 /**
  * @param[trackWidth] distance between wheels on opposite sides; see the diagram below
  * ![Wheelbase and track width diagram](https://upload.wikimedia.org/wikipedia/commons/5/52/Wheelbase_and_Track.png)
+ * @param[wheelbase] distance between wheels on the same side; see the diagram in [MecanumKinematics]
+ */
+data class SwerveKinematics @JvmOverloads constructor(
+    @JvmField
+    val trackWidth: Double,
+    @JvmField
+    val wheelBase: Double
+) {
+    constructor(
+        trackWidth: Double
+    ) : this(trackWidth, trackWidth)
+
+    data class WheelIncrements<Param>(
+        @JvmField
+        val leftFront: Vector2dDual<Param>,
+        @JvmField
+        val leftBack: Vector2dDual<Param>,
+        @JvmField
+        val rightBack: Vector2dDual<Param>,
+        @JvmField
+        val rightFront: Vector2dDual<Param>,
+    )
+
+    fun <Param> forward(w: WheelIncrements<Param>) = Twist2dDual(
+        Vector2dDual(
+            (w.leftFront.x + w.leftBack.x + w.rightBack.x + w.rightFront.x) * 0.25,
+            (w.leftFront.y + w.leftBack.y + w.rightBack.y + w.rightFront.y) * 0.25
+        ),
+        (
+                (-w.leftFront.x - w.leftBack.x + w.rightBack.x + w.rightFront.x) * 0.5 * trackWidth +
+                ( w.leftFront.y - w.leftBack.y - w.rightBack.y + w.rightFront.y) * 0.5 * wheelBase
+        ) / (wheelBase * wheelBase + trackWidth * trackWidth)
+    )
+
+    data class WheelVelocities<Param>(
+        @JvmField
+        val leftFront: Vector2dDual<Param>,
+        @JvmField
+        val leftBack: Vector2dDual<Param>,
+        @JvmField
+        val rightBack: Vector2dDual<Param>,
+        @JvmField
+        val rightFront: Vector2dDual<Param>,
+    ) {
+        fun all() = listOf(leftFront, leftBack, rightBack, rightFront)
+    }
+
+    fun <Param> inverse(t: PoseVelocity2dDual<Param>) = WheelVelocities(
+        Vector2dDual(t.linearVel.x - t.angVel * trackWidth * 0.5, t.linearVel.y + t.angVel * wheelBase * 0.5),
+        Vector2dDual(t.linearVel.x - t.angVel * trackWidth * 0.5, t.linearVel.y - t.angVel * wheelBase * 0.5),
+        Vector2dDual(t.linearVel.x + t.angVel * trackWidth * 0.5, t.linearVel.y - t.angVel * wheelBase * 0.5),
+        Vector2dDual(t.linearVel.x + t.angVel * trackWidth * 0.5, t.linearVel.y + t.angVel * wheelBase * 0.5)
+    )
+
+    inner class WheelVelConstraint(@JvmField val maxWheelVel: Double) : VelConstraint {
+        override fun maxRobotVel(robotPose: Pose2dDual<Arclength>, path: PosePath, s: Double): Double {
+            val txRobotWorld = robotPose.value().inverse()
+            val robotVelWorld = robotPose.velocity().value()
+            val robotVelRobot = txRobotWorld * robotVelWorld
+            return inverse(PoseVelocity2dDual.constant<Arclength>(robotVelRobot, 1))
+                .all()
+                .minOf { abs(maxWheelVel / it.value().norm()) }
+        }
+    }
+}
+
+/**
+ * @param[trackWidth] distance between wheels on opposite sides; see the diagram below
+ * ![Wheelbase and track width diagram](https://upload.wikimedia.org/wikipedia/commons/5/52/Wheelbase_and_Track.png)
  */
 data class TankKinematics(@JvmField val trackWidth: Double) {
     data class WheelIncrements<Param>(
