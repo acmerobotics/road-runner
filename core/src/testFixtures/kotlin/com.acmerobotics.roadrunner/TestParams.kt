fcda25d0c211bf29834aa09731ac6d0f5759c0ec
Ryan Brott <rcbrott@gmail.com>
2024-03-15 22:50:59 -0700

    Add test fixtures

---
+++ core/src/testFixtures/kotlin/com.acmerobotics.roadrunner/TestParams.kt
@@ -0,0 +1,12 @@
+package com.acmerobotics.roadrunner
+
+val TEST_PROFILE_PARAMS = ProfileParams(
+    0.25,
+    0.1,
+    1e-4,
+)
+
+val TEST_TRAJECTORY_BUILDER_PARAMS = TrajectoryBuilderParams(
+    1e-6,
+    TEST_PROFILE_PARAMS
+)
